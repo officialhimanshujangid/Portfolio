ad84496f1745f44425d50addbe2b68a0219ad712
HimanshuJangidofficial <officialhimanshujangid>
2024-02-21 15:32:31 +0530

    commit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,20 @@
 function Project() {
-  return <div>Project</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci omnis
+      voluptate doloremque officia, eius soluta sint eos sit exercitationem
+      asperiores consectetur corporis iusto impedit quibusdam deserunt eum
+      aliquid ducimus illum quod natus sequi beatae, temporibus vero saepe?
+      Cupiditate modi deleniti voluptatem aliquid ab iste tempore molestias
+      autem non, quisquam quis laudantium exercitationem perferendis, error, ut
+      blanditiis magnam. Sunt rem dolorem excepturi, quo, ab deleniti nemo
+      explicabo eius veritatis molestiae autem fugiat voluptate. Aliquam,
+      distinctio perferendis cumque in voluptatum laborum repellendus quas.
+      Voluptatum sapiente ullam, itaque molestias qui obcaecati sunt earum nisi,
+      dolor saepe exercitationem velit, repudiandae quasi voluptatem fugiat
+      enim.
+    </div>
+  );
 }
 
 export default Project;

5eca7cf7eebefc0301be4562e757ce779882e54b
HimanshuJangidofficial <officialhimanshujangid>
2024-02-21 15:32:14 +0530

    commit

--- src/pages/Additional.jsx
+++ src/pages/Additional.jsx
@@ -1,5 +1,29 @@
 function Additional() {
-  return <div>certification</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quidem
+      voluptas perferendis atque quibusdam et distinctio dolore architecto!
+      Mollitia optio voluptates nobis voluptatem quasi quidem libero eveniet eum
+      deleniti architecto asperiores animi, vel, facilis unde explicabo quis
+      aliquam nisi eligendi. Explicabo officia iure tenetur. Minima suscipit
+      iusto nam saepe alias temporibus quod, molestiae ab harum odio explicabo,
+      distinctio praesentium eaque tempore, dignissimos minus dolorem eos rem
+      deserunt. Voluptates cupiditate repellendus illo perferendis doloremque et
+      laboriosam repudiandae dolorem quas iure, expedita modi quasi similique?
+      Tenetur alias velit quidem debitis provident eligendi vel ad
+      necessitatibus assumenda tempore. Placeat enim ad animi repudiandae veniam
+      quidem dolor asperiores explicabo tempora doloremque error, sint quae cum
+      ipsa vel unde consequatur nemo officia, saepe eligendi eaque iusto
+      temporibus? Adipisci vero vitae assumenda eius odio vel rerum modi facilis
+      quia id ducimus dolorum porro, inventore error? Delectus eaque assumenda
+      culpa possimus pariatur odit soluta porro accusantium tempore beatae ipsum
+      earum tenetur, corporis nemo itaque cupiditate libero, minus optio illo
+      saepe quibusdam sint. Ea ad impedit aliquid sint voluptate id ullam et,
+      laboriosam quibusdam quasi, corporis esse veniam sapiente numquam velit
+      nulla. Quisquam nisi eos vel repudiandae iusto ipsum minus, sunt
+      cupiditate deleniti quia ut id illo minima?
+    </div>
+  );
 }
 
 export default Additional;
--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,5 +1,29 @@
 function Education() {
-  return <div>education</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore cupiditate
+      non nam sunt consequuntur est, quasi magnam corrupti, sed soluta quos?
+      Dignissimos odio qui obcaecati, facilis quisquam ducimus temporibus est!
+      Dolore eaque quam dolorum excepturi reprehenderit, quos ea, atque at
+      mollitia deserunt aut quasi veritatis reiciendis vero, quidem illo iste
+      molestiae delectus numquam omnis inventore. Necessitatibus aspernatur in
+      quos debitis facere eveniet possimus rerum cum, dicta molestias, suscipit,
+      aut deserunt? Saepe alias numquam voluptatum culpa beatae itaque dolorum
+      autem harum eius aperiam, amet expedita. Eius corporis eos hic dolore
+      obcaecati odio corrupti sapiente vel ipsum laborum, quos aliquam,
+      repellendus molestiae, libero laboriosam reprehenderit natus! Qui vero non
+      molestias quaerat, neque laborum debitis, quidem exercitationem,
+      cupiditate eaque hic odit ducimus ad nobis! Et, nam non. Vel, in, odit
+      nesciunt quasi consequuntur quod eos cumque illo voluptatem esse eligendi
+      adipisci fuga repudiandae. Nihil earum aspernatur doloribus quidem?
+      Reiciendis atque, dignissimos ab ullam tempora natus animi! Ratione
+      distinctio error neque ut natus asperiores debitis perspiciatis iste sequi
+      deserunt? Veniam velit sequi ab ullam quas aut iure iusto sunt, delectus
+      similique laborum, error non facere nobis maxime nemo excepturi voluptate.
+      Soluta, consequatur. Consequatur sunt voluptatum totam tenetur ut eos
+      libero at nisi ducimus doloribus.ducation
+    </div>
+  );
 }
 
 export default Education;
